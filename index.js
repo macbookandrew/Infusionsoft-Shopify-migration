bb9493234a6ead65cf28b1436e68c29491c0070b
Andrew Minion <andrew@andrewrminion.com>
2017-04-18 19:55:05 -0400

    simplify mysql and shopify

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 // set up variables and MySQL
 var config = require('./config'),
     mysql = require('mysql'),
-    localDatabase = mysql.createConnection({
-        host:        config.mysql.host,
-        user:        config.mysql.username,
-        password:    config.mysql.password,
-        database:    config.mysql.database
-    }),
+    localDatabase = mysql.createConnection(config.mysql),
     Shopify = require('shopify-api-node'), // see https://github.com/MONEI/Shopify-api-node#available-resources-and-methods
-    shopify = new Shopify({
-        shopName:   config.shopify.shopName,
-        apiKey:     config.shopify.apiKey,
-        password:   config.shopify.password,
-        autoLimit:  config.shopify.autoLimit
-    }),
     json = '';
+    shopify = new Shopify(config.shopify),
 
 localDatabase.connect();
 
